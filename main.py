76418e56570d766702ccba1dabd1097496665368
alicethefemme <alicethefemme@thepenguinclub.xyz>
2025-01-24 09:01:21 +0000

    Changed the MDI window into a plot graph.

--- main.py
+++ main.py
@@ -1,127 +1,127 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLCDNumber, QMenu, QAction, QDialog, QToolBox, QWidget, QLabel, QDesktopWidget, QToolBar, QPushButton, QMdiArea, QMdiSubWindow, QListWidget
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLCDNumber, QMenu, QAction, QDialog, QToolBox, QWidget, QLabel, \
+    QDesktopWidget, QToolBar, QPushButton, QMdiArea, QMdiSubWindow, QListWidget
 from PyQt5 import QtCore, uic
 from PyQt5.QtGui import *
 
-class mySubWindow(QMainWindow):
+import pyqtgraph as pg
+
+
+class mySubWindow(QMainWindow):  # Window that is produced from the toolbar
 
     def __init__(self):
-
         super().__init__()
 
-        uic.loadUi("subwindow.ui",self)
+        uic.loadUi("subwindow.ui", self)  # Load the UI file and grab its XML.
 
         # get widget references from the ui file
-        self.btnClose = self.findChild(QPushButton,"btnSubClose")
+        self.btnClose = self.findChild(QPushButton, "btnSubClose")
         print(self.btnClose)
-        
-        #Events from sub window
+
+        # Events from sub window
         self.btnClose.clicked.connect(self.closeSubWindow)
 
         self.setupWidgets()
-        self.setWindowModality(QtCore.Qt.ApplicationModal)
-        
-        self.show()
+        self.setWindowModality(
+            QtCore.Qt.ApplicationModal)  # Sets the window to be a modal, so that you can't click back to the main application.
 
-    #enddef  
+        self.show()  # Display the subwindow.
+
+    # enddef
 
     def closeSubWindow(self):
+        self.close()  # Closes the child window.
 
-        self.close()
-        
-    #end def
+    # end def
 
     def setupWidgets(self):
-        
+        # Set the title of the window created.
         self.setWindowTitle("SubWindow")
-  
+
         # setting  the geometry of window
         self.setGeometry(299, 200, 400, 300)
-  
+
         # creating a label widget
         # by default label will display at top left corner
         self.label = QLabel('This is label', self)
 
-    #enddef  
-
-#endclass mywindow
+    # enddef
 
 
-class mainWindow(QMainWindow):
+# endclass mywindow
+
+
+class mainWindow(QMainWindow):  # The main window that opens when the application starts
 
     def __init__(self):
-
         super().__init__()
-        uic.loadUi("mainui.ui",self)
+        uic.loadUi("mainui.ui", self)  # Load the file
 
+        # Set the number for how many windows are open.
         self.mdi_windows_open = 0
 
+        # Create all the defauls and enable them.
         self.set_window_properties()
         self.create_menus()
         self.create_toolbox()
         self.create_statusbar()
 
         # get widget references from the ui file
-        self.btnOk = self.findChild(QPushButton,"btnOpen")
-        self.mdi = self.findChild(QMdiArea,"mdiArea")
-        self.lcd = self.findChild(QLCDNumber,"lcdNumber")
+        self.btnOk = self.findChild(QPushButton, "btnOpen")
+        self.mdi = self.findChild(QMdiArea, "mdiArea")
+        self.lcd = self.findChild(QLCDNumber, "lcdNumber")
         self.lcd.setDigitCount(2)
 
-        #Events from main window
+        # Events from main window
         self.btnOk.clicked.connect(self.new_mdiWindow)
 
-    #enddef
-
+    # enddef
 
     def set_window_properties(self):
-
         # set window size and position settings
         self.height = 300
         self.width = 300
         self.top = 150
         self.left = 150
 
-    #enddef  
-
+    # enddef
 
     def create_menus(self):
-
         # Menu Bar Settings
         action = self.findChild(QAction, "menu_WindowNew")
         action.triggered.connect(self.openSubWindow)
-                
-    #enddef  
 
+    # enddef
 
     def openSubWindow(self):
-
         self.newWin = mySubWindow()
-        
-    #enddef  
 
+    # enddef
 
     def create_toolbox(self):
-
         pass
 
-    #enddef  
-
+    # enddef
 
     def create_statusbar(self):
-        
         # Status Bar Settings
         self.statusBar = self.statusBar()
         self.statusBar.showMessage("This is an status message.", 5000)
         label = QLabel("permanent status")
         self.statusBar.addPermanentWidget(label)
 
-    #enddef  
-
+    # enddef
 
     def new_mdiWindow(self):
-    
         new = QMdiSubWindow()
-        new.setWidget(QListWidget())
+
+        # graph = pg
+        x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        y = [1, 2, 3, 4, 5, 4, 3, 2, 1, 0]
+        #
+        # graph.plot(x, y)
+
+        new.setWidget(pg.plot(x, y))
         new.setWindowTitle("new")
         self.mdi.addSubWindow(new)
 
@@ -131,11 +131,11 @@ class mainWindow(QMainWindow):
         self.lcd.update()
 
         new.show()
-        
-    #enddef  
-        
 
-#endclass MainWindow
+    # enddef
+
+
+# endclass MainWindow
 
 
 # main starts here
